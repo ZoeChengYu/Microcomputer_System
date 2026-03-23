bc1829fc24e35eea8f8ee671b2097171123a8166
ZoeChengYu <0931426632yang50@gmail.com>
2026-03-23 18:04:12 +0800

    Add seven segment display logic and simulation files
    - Created VHDL file for seven segment display logic (seven_displays.vhd) implementing segment control based on switch inputs.
    - Added timing analyzer summary file (Lab_03.sta.summary) for project timing analysis.
    - Introduced Questa simulation configuration files (Lab_03.sft and Lab_03.vo) for FPGA simulation.

---
+++ Lab_03/BCD_pg.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+package BCD_pg is
+    component BCD4
+        port(
+            A,B:in std_logic_vector (3 downto 0);
+            S: out std_logic_vector (3 downto 0);
+            Ci:in std_logic;
+            Co:out std_logic
+        );
+    end component BCD4;
+    
+    component seven_displays is
+        port(
+            sw0,sw1,sw2,sw3,sw4,sw5,sw6,sw7:in std_logic;
+            a0,a1,a2,a3,a4,a5,a6,b0,b1,b2,b3,b4,b5,b6:out std_logic
+        );
+    end component seven_displays;
+end package;
---
+++ Lab_03/Lab_03.vhd
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use work.BCD_pg.all;
+
+entity Lab_03 is
+    port(
+        A,B:in std_logic_vector (3 downto 0);
+        S: out std_logic_vector (3 downto 0);
+        Co:out std_logic_vector (1 downto 0)
+        );
+end entity;
+
+architecture main of Lab_03 is
+    begin
+
+end architecture;
