a94b1814fc1b2f06120a751dd7e0ba898f61278d
ZoeChengYu <0931426632yang50@gmail.com>
2026-03-23 18:04:12 +0800

    Add seven segment display logic and simulation files
    - Created VHDL file for seven segment display logic (seven_displays.vhd) implementing segment control based on switch inputs.
    - Added timing analyzer summary file (Lab_03.sta.summary) for project timing analysis.
    - Introduced Questa simulation configuration files (Lab_03.sft and Lab_03.vo) for FPGA simulation.

---
+++ Lab_03/BCD4.vhd
@@ -0,0 +1,37 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use work.FA_pg.all;
+
+entity BCD4 is
+    port(
+        A,B:in std_logic_vector (3 downto 0);
+        S: out std_logic_vector (3 downto 0);
+        Ci:in std_logic;
+        Co:out std_logic
+    );
+end entity;
+
+architecture base of BCD4 is
+    signal carry:std_logic_vector(3 downto 0);
+    signal Tcarry:std_logic_vector(3 downto 0);
+    signal sum:std_logic_vector(3 downto 0);
+    signal Tsum:std_logic_vector(3 downto 0);
+    signal bcdmain:std_logic;
+    begin
+        stage0:FA port map(A(0),B(0),ci,Tsum(0),Tcarry(0));
+        stage1:FA port map(A(1),B(1),Tcarry(0),Tsum(1),Tcarry(1));
+        stage2:FA port map(A(2),B(2),Tcarry(1),Tsum(2),Tcarry(2));
+        stage3:FA port map(A(3),B(3),Tcarry(2),Tsum(3),Tcarry(3));
+        stage4:FA port map(Tsum(0),'0','0',sum(0),carry(0));
+        stage5:FA port map(Tsum(1),bcdmain,carry(0),sum(1),carry(1));
+        stage6:FA port map(Tsum(2),bcdmain,carry(1),sum(2),carry(2));
+        stage7:FA port map(Tsum(3),'0',carry(2),sum(3),carry(3));
+        
+        bcdmain<=Tcarry(3) or (Tsum(3) and Tsum(2)) or (Tsum(3) and Tsum(1));
+
+        Co<=bcdmain;
+        S(0)<=sum(0);
+        S(1)<=sum(1);
+        S(2)<=sum(2);
+        S(3)<=sum(3);
+end architecture;
