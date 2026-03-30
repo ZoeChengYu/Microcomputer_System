023a1eb4a44d81b8bbd892eb45b00e5ea99d7c45
ZoeChengYu <0931426632yang50@gmail.com>
2026-03-30 17:06:41 +0800

    Add user manual and Lab 03 workspace file
    - Added DE2_115_User_manual.pdf (14048 KB) to the repository.
    - Created new Lab_03.qws file with initial workspace settings.

--- Lab_03/Lab_03.vhd
+++ Lab_03/Lab_03.vhd
@@ -7,7 +7,7 @@ entity LAB_03 is
         A,B: in std_logic_vector (7 downto 0);
         a0, b0, c0, d0, e0, f0, g0: out std_logic;
         a1, b1, c1, d1, e1, f1, g1: out std_logic;
-        overflow: out std_logic
+        overflow0,overflow1: out std_logic
     );
 end entity LAB_03;
 
@@ -15,6 +15,7 @@ architecture structure of LAB_03 is
     signal sum_low: std_logic_vector(3 downto 0);
     signal sum_high: std_logic_vector(3 downto 0);
     signal carry_mid: std_logic;
+	 signal s_o : std_logic;
 begin
     -- 處理個位數的 BCD 加法
     stage0: BCD4 port map (
@@ -31,7 +32,7 @@ begin
         B(7 downto 4), 
         carry_mid, 
         sum_high, 
-        overflow
+        s_o
     );
 
     -- 個位數七段顯示器 (HEX0)
@@ -45,5 +46,7 @@ begin
         w => sum_high(3), x => sum_high(2), y => sum_high(1), z => sum_high(0),
         a => a1, b => b1, c => c1, d => d1, e => e1, f => f1, g => g1
     );
+	 overflow0<=not(s_o);
+	 overflow1<=not(s_o);
 
 end architecture structure;
